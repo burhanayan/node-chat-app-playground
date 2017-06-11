1f4803b7b6b6cc08ad10d646cb83573dbc84e03d
burhanayan <1burhan.ayan@gmail.com>
2017-06-12 01:12:27 +0300

    initial commit

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+var app = express();
+const port = process.env.PORT || 3000;
+
+const publicPath = path.join(__dirname + '/../public');
+
+console.log(__dirname + '/../public');
+console.log(publicPath);
+
+app.use(express.static(publicPath));
+
+app.listen(port, function() {
+    console.log(`App listening on port ${port}`);
+});
