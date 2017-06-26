ac593dc9e53a8f430a535c7d8a215fe8b428ab72
Burhan Ayan <1burhan.ayan@gmail.com>
2017-06-27 00:18:05 +0300

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -16,25 +16,7 @@ socket.on('newMessage', function (message) {
     jQuery('#messages').append(li);
 });
 
-socket.emit('createMessage', {
-    from: 'Ahmet',
-    text: 'Hi!'
-}, function (data) {
-    console.log('Got it!', data);
-});
-
-jQuery('#message-form').on('submit', function (e) {
-    e.preventDefault();
-
-    socket.emit('createMessage', {
-        from: 'User',
-        text: jQuery('[name=message]').val()
-    }, function () {
-        
-    });
-});
-
-socket.on('newLocationMessage', function(message) {
+socket.on('newLocationMessage', function (message) {
     var li = jQuery('<li></li>');
     var a = jQuery('<a target="_blank">My current location</a>');
 
@@ -44,18 +26,36 @@ socket.on('newLocationMessage', function(message) {
     jQuery('#messages').append(li);
 });
 
+jQuery('#message-form').on('submit', function (e) {
+    e.preventDefault();
+
+    var messageTextBox = jQuery('[name=message]');
+
+    socket.emit('createMessage', {
+        from: 'User',
+        text: jQuery('[name=message]').val()
+    }, function () {
+        messageTextBox.val('')
+    });
+});
+
+
 var locationButton = jQuery('#send-location');
 locationButton.on('click', function () {
-    if (!navigator.geolocation){
+    if (!navigator.geolocation) {
         return alert('Geolocation not supported by your browser.');
     }
 
+    locationButton.attr('disabled','disabled').text('Sending location...');
+
     navigator.geolocation.getCurrentPosition(function (position) {
+        locationButton.removeAttr('disabled').text('Send location');;
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         });
     }, function () {
+        locationButton.removeAttr('disabled').text('Send location');
         alert('Unable to fetch location.');
     });
 });
--- server/server.js
+++ server/server.js
@@ -25,12 +25,11 @@ io.on('connection', (socket) => {
     socket.on('createMessage', (createdMessage, callback) => {
         console.log('Created Message', createdMessage);
         io.emit('newMessage', generateMessage(createdMessage.from, createdMessage.text));
-        callback('This is from the server');
+        callback();
+    });
 
-        socket.on('createLocationMessage', (coords) => {
-            io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
-        });
-
+    socket.on('createLocationMessage', (coords) => {
+        io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
     });
 
     socket.on('disconnect', () => {
